9d33ff0c1135e5b24dd496351b6398e6a7b8f1a1
ahamdy997 <bme.ahmedhamdy@gmail.com>
2023-04-30 20:06:00 +0300

    PortoCommit

---
+++ JS/main.js
@@ -0,0 +1,334 @@
+// //  Problem N1
+
+// // var x = window.prompt("Please Enter An Input");
+// // console.log(x);
+
+// // problem N2
+
+// // var x =window.prompt("Please Enter An Input");
+// // if (x%4 ==0 || x%3==0){
+// //   console.log("Yes");
+// // }else{
+// //   console.log("No");
+// // }
+
+// // problem N3
+
+// // var x =window.prompt("Please Enter First Number");
+// // var y =window.prompt("Please Enter Second Number");
+// // if (x>y) {
+// //   console.log(x + " Is Bigger Than " +y);
+// // } else {
+// //   console.log(y + " Is Bigger Than " +x);
+// // }
+
+// // problem N4
+
+// // var x =window.prompt("Please Enter Number");
+// // if (x<0) {
+// //   console.log(x+" Is A Negative Number");
+// // } else {
+// //   console.log(x+" Is A Positive Number");
+// // }
+
+
+
+// // Problem N5
+
+// // var x =window.prompt("Please Enter First Number");
+// // var y =window.prompt("Please Enter Second Number");
+// // var z =window.prompt("Please Enter Third Number");
+
+// // if (x>y && x>z && y>z) {
+// //   console.log(x+" Is The Maximum Number");
+// //   console.log(z+" Is The Minimum Number");
+// // } else if (y>x && y>z && x>z) {
+// //   console.log(y+" Is The Maximum Number");
+// //   console.log(z+" Is The Minimum Number");
+// // }else if(x>y && x>z && y<z) {
+// //   console.log(x+" Is The Maximum Number");
+// //   console.log(y+" Is The Minimum Number");
+// // }else if(y>x && y>z && x<z) {
+// //   console.log(y+" Is The Maximum Number");
+// //   console.log(x+" Is The Minimum Number");
+// // }else{
+// //   console.log(z+" Is The Maximum Number");
+// //   if (x<y) {
+// //   console.log(x+" Is The Minimum Number");
+// //   }
+// //   if (y<x) {
+// //   console.log(y+" Is The Minimum Number");
+// //   }
+// // }
+
+
+
+// // Problem N6
+// // var x = window.prompt("Please Enter A Number");
+// // if (x%2==0) {
+// //   console.log("The Number Is Even");
+// // } else {
+// //   console.log("The Number Is Odd");
+// // }
+
+
+// // Problem N7
+
+// // var x = window.prompt("Please Enter Character");
+// // if (x=="a" || x=="e" || x=="i" || x=="o" || x=="u") {
+// //   console.log("Vowel");
+// // } else {
+// //   console.log("Consonant");
+// // }
+
+
+
+// // Problem N8
+
+// // var x= window.prompt("Please Enter A Number");
+// // for ( i = 1; i <= x; i++) {
+// //   console.log(i);
+// // }
+
+
+
+// // Problem N9
+
+// // var x =window.prompt("Please Enter A Number");
+// // for(i=1;i<=12; i++){
+// //   console.log(x*i);
+// // }
+
+
+// // Problem N10
+
+// // var x =window.prompt("Please Enter A Number");
+// // for(i=1; i<=x;i++){
+// //   if (i%2==0) {
+// //   console.log(i);
+// //   }
+// // }
+
+
+// // Problem N11
+
+// // var x=window.prompt("Please Enter The Number");
+// // var y=window.prompt("Please Enter The Power");
+// // var z =x**y;
+// // console.log(z);
+
+
+
+// // Problem N12
+// // var x=Number(window.prompt("Please Enter Subject 1 Grade")) ;
+// // var y=Number(window.prompt("Please Enter Subject 2 Grade"));
+// // var z=Number(window.prompt("Please Enter Subject 3 Grade"));
+// // var h=Number(window.prompt("Please Enter Subject 4 Grade"));
+// // var w=Number(window.prompt("Please Enter Subject 5 Grade"));
+
+// // var total=  x + y + z + h + w;
+// // console.log("Total Marks = " +total);
+// // var avg=(x+y+z+h+w)/5;
+// // console.log("Average Marks = "+avg);
+// // console.log("Precentage  = "+avg);
+
+
+
+// // Problem N13
+
+// // var x =window.prompt("Please Enter Month Number");
+// // if (x>12 || x<=0) {
+// //   console.log("Error");
+// // }
+// // else if (x==1) {
+// //   console.log("Days In The Month : 31");
+// // }else if(x==2)
+// // {
+// //   console.log("Days In The Month : 28");
+// // }
+// // else {
+// //   console.log("Days In The Month : 30");
+// // }
+
+
+
+// // Problem N14
+
+// // var x =Number(window.prompt("Please Enter Your  Physics Grade")); 
+// // var y =Number(window.prompt("Please Enter Your Chemistry Grade"));
+// // var z =Number(window.prompt("Please Enter Your Biology Grade"));
+// // var s =Number(window.prompt("Please Enter Your Mathematics Grade"));
+// // var a =Number(window.prompt("Please Enter Your Computer Grade"));
+// // var avg= (x+y+z+s+a)/5;
+// // if (avg>= 90) {
+// // console.log("Percentage: "+avg+" Grade A");
+// // } else if (avg>= 80) {
+// // console.log("Percentage: "+avg+" Grade B"); 
+// // } else if( avg>= 70){
+// // console.log("Percentage: "+avg+" Grade B"); 
+// // } else if( avg>= 60){
+// //   console.log("Percentage: "+avg+" Grade C"); 
+// //   }
+// //   else if( avg>= 40){
+// //     console.log("Percentage: "+avg+" Grade E"); 
+// //   } 
+// //   else if( avg< 40){
+// //       console.log("Percentage: "+avg+" Grade F"); 
+// //   }
+
+
+// // Problem N15
+// // var x =window.prompt("Please Enter Month Number");
+// // switch (x) {
+// //   case "1":console.log("31 Days");
+// //     break;
+// //     case "2":console.log("28 Days");
+// //     break;
+// //   default: console.log("30 Days");
+// //     break;
+// // }
+
+
+// // Problem N16
+
+// // var x =window.prompt("Please Enter A Character");
+// // switch (x) {
+// //   case "a":console.log("Vowel");
+// //     break;
+// //     case "i":console.log("Vowel");
+// //     break;
+// //     case "e":console.log("Vowel");
+// //     break;
+// //     case "o":console.log("Vowel");
+// //     break;
+// //     case "u":console.log("Vowel");
+// //     break;
+// //   default:console.log("consonant");
+// //     break;
+// // }
+
+
+// // Problem N17
+// // var num1 = window.prompt("Please Enter First Number");
+// // var num2 = window.prompt("Please Enter Second Number");
+
+// // switch (num1>num2) {
+// //     case false:
+// //       console.log(num2);
+// //       break;
+// //     case true:
+// //       console.log(num1);
+// //     break;
+// //     }
+
+// // Problem N18
+
+// // var x = window.prompt("Please Enter A Number");
+
+// // switch (x%2==0) {
+// //   case true: console.log("Even Number");
+// //     break;
+// //   case false:console.log("Odd Number");
+
+// // }
+
+
+// // Problem N19
+
+// // var x = window.prompt("Please Enter A Number");
+
+// // switch (x>0) {
+// //   case true:console.log("Positive Number");
+// //     break;
+// //     case false:console.log("Negative Number");
+// //     break;
+// //   default:console.log("Number = 0");
+// //     break;
+// // }
+
+
+// // Assignment N20
+// var result;
+
+
+// var number1 =Number( window.prompt('Enter First Number: '));
+// var number2 = Number(window.prompt('Enter Second Number: '));
+
+// var operator = window.prompt('Enter operator ( either +, -, * or / ): ');
+
+// switch(operator) {
+//     case '+':
+//         result = number1 + number2;
+//         console.log(result);
+//         break;
+
+//     case '-':
+//         result = number1 - number2;
+//         console.log(result);
+//         break;
+
+//     case '*':
+//          result = number1 * number2;
+//         console.log(result);
+//         break;
+
+//     case '/':
+//         result = number1 / number2;
+//         console.log(result);
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+// let a=10 , b= "20", c=80;
+// console.log(++a +b++ + +c++ - +a++);
+// console.log(11+20+80-11);
+// console.log(++a -b +  +c++ - -a++ + +a);
+// console.log(13 - 21 + 81+13+14 );
+
+// console.log(--c + +b + --a * + b++ - +b*a + --a - +true);
+// console.log(81+21+13*21 - 22*13 + 12-1);
+
+// let d="-100" , e= "20", f=30 , g=true;
+
+// console.log(++e * ++g -d + ++f)
+
+
+
+let a=1_00;
+let b=2_00.5;
+let c=1e2;
+let d=2.4;
+
+console.log(Math.round(Math.min(a,b,c,d)));
+console.log(Math.pow(a,Math.round(d)));
+console.log(Math.round(d));
+console.log(Math.floor(d));
+console.log(Math.trunc(d));
+console.log(parseInt(d));
+console.log((Math.floor(b)/Math.ceil(d)).toString());
+console.log(Math.round((Math.floor(b)/Math.ceil(d))));
+
+
+
+
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'JS/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+
+  
+  $(".togglex").click(function(){
+      $(".social").toggle(
+    function(){$(".social").css({"opacity": "1"});},
+    function(){$("social").css({"opacity": "0"});})
+  });
+
